e50a715df074fd90f94bb08cd66e7841a00150dd
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2014-12-21 20:55:24 +0300

    Added models

--- src/filenames.h
+++ src/filenames.h
@@ -2,7 +2,7 @@
 #define FILENAMES_H
 
 
-char const * const MODEL_FILE = "models/model.obj";
+char const * const MODEL_FILE = "models//sphere.obj";
 char const * const WIREFRAME_VS_FILE = "shaders//hw1_wireframe.glslvs";
 char const * const WIREFRAME_FS_FILE = "shaders//hw1_wireframe.glslfs";
 
